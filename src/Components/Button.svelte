ed221e32cb1a82b1e3e9ee914ce5b88c9b87eebd
B33Gs <77767256+B33Gs@users.noreply.github.com>
2022-01-07 02:13:30 -0500

    <Button /> component takes onclick and text

---
+++ src/Components/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let onClick;
+	export let buttonText;
+	import { colors } from '../globalColors';
+
+	const orange = colors.orange;
+	const purple = colors.purple;
+</script>
+
+<button on:click={onClick} style="--theme-orange: {orange}; --theme-purple: {purple} "
+	>{buttonText}</button
+>
+
+<style>
+	button {
+		background-color: var(--theme-orange);
+		color: var(--theme-purple);
+		width: 295px;
+		height: 40px;
+		margin: 25px 0;
+		font-size: 25px;
+		font-weight: bold;
+		border: none;
+		border-radius: 10px;
+	}
+	button:hover {
+		cursor: pointer;
+	}
+</style>
