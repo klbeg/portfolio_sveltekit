54def485f31754fa83cc4254c85b51f7d6b252e1
B33Gs <77767256+B33Gs@users.noreply.github.com>
2022-01-13 13:38:37 -0500

    added color variables to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@ module.exports = {
 	purge: ['./src/**/*.html', './src/**/*.svelte'],
 	content: [],
 	theme: {
+		colors: {
+			orange: '#ff9e00',
+			white: '#fff',
+			purple: '#240046',
+			darkPurple: '#1d0036'
+		},
 		extend: {}
 	},
 	plugins: []
