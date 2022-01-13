201a2f9a971aab18a95980841a36f4073b6368eb
B33Gs <77767256+B33Gs@users.noreply.github.com>
2022-01-13 18:37:16 -0500

    re-styled index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,17 @@
 <script lang="ts">
 	import WorkPreview from '../Components/WorkPreview.svelte';
-	import { colors } from '../globalColors';
-
-	const orange = colors.orange;
 </script>
 
-<div class="flex-center" style="--theme-orange: {orange}">
+<div class="flex flex-col items-center">
 	<div class="content-container">
-		<h2>Hi, I'm Kevin!</h2>
+		<h2 class="text-3xl mb-10">Hi, I'm Kevin!</h2>
 		<p>
 			JavaScript full stack developer, musician, and witty team player seeking fulltime employment.
 		</p>
 	</div>
 	<div class="content-container">
-		<h1>My Work:</h1>
+		<h1 class="text-orange text-2xl">My Work:</h1>
 		<!-- import projects array.  Map projects and pass in project info as prop -->
 		<WorkPreview />
 	</div>
 </div>
-
-<style>
-	h1 {
-		font-family: 'Montserrat', sans-serif;
-		color: var(--theme-orange);
-	}
-	h2 {
-		padding-bottom: 50px;
-		font-size: 25px;
-	}
-</style>
