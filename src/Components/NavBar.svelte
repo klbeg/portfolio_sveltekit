aa448fd3ebdf287b1d8b0029dbde730cdf738152
B33Gs <77767256+B33Gs@users.noreply.github.com>
2022-01-13 17:24:46 -0500

    NavBar re-styled with tailwind

--- src/Components/NavBar.svelte
+++ src/Components/NavBar.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	export let showNav;
 	export let toggleNav;
+
+	const links: string[][] = [
+		['home', '/'],
+		['about', '/about'],
+		['contact', '/contact']
+	];
 </script>
 
-<nav class={!showNav ? 'hide' : ''}>
-	<ul>
-		<a href="/" on:click={toggleNav}>
-			<li>HOME</li>
-		</a>
-		<a href="/about" on:click={toggleNav}>
-			<li>ABOUT</li>
-		</a>
-		<a href="/contact" on:click={toggleNav}>
-			<li>CONTACT</li>
-		</a>
+<nav class={!showNav && 'hide'}>
+	<ul class="flex flex-col">
+		{#each links as link}
+			<a class="uppercase text-xl" href={link[1]} on:click={toggleNav}>
+				<li class="py-2 flex justify-center my-0.5 w-40 text-xl">{link[0]}</li>
+			</a>
+		{/each}
 	</ul>
 </nav>
 
@@ -26,28 +28,10 @@
 	.hide {
 		height: 0;
 	}
-	ul {
-		list-style: none;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding-bottom: 30px;
-	}
 	ul li {
-		display: flex;
-		justify-content: center;
-		padding: 8px;
-		margin: 2px 0;
-		width: 100px;
-		font-size: 20px;
 		border: 1px solid transparent;
 		border-radius: 5px;
 		transition: all 0.5s ease-in-out;
-	}
-	a {
-		text-decoration: none;
-		color: #fff;
 	}
 	ul li:hover {
 		cursor: pointer;
