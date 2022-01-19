cbb9529774baae3d69a36552e3fe2cfd89fecfa8
B33Gs <77767256+B33Gs@users.noreply.github.com>
2022-01-18 20:50:16 -0500

    created basic <CaseStudy /> & route for movieCraze

---
+++ src/routes/movieCraze.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import CaseStudy from '../Components/CaseStudy.svelte';
+</script>
+
+<div class="flex justify-center">
+	<div class="content-container">
+		<!-- should take prop of projectCaseStudy -->
+		<CaseStudy />
+	</div>
+</div>
