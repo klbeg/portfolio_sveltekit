95bc643ccf55c6081650e2f43a970b71cef17443
B33Gs <speschk@yahoo.com>
2025-05-13 17:19:34 -0400

    add functionality for form to send email

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
 	import Link from "../Components/Link.svelte"
+	import Button from "../Components/Button.svelte"
 </script>
 
-<!-- <div class="flex flex-col items-center"> -->
 <div class="content-container">
-	<h1 class="text-2xl">Interested in <br /> Working Together?</h1>
+	<h1 class="text-2xl mb-10">Interested in <br /> Working Together?</h1>
+	<form action="https://formspree.io/f/xdoyjkro" method="POST" class="pt-2">
+		<label class="my-9 text-2xl" for="email">
+			Your email:
+			<input class="mt-9 mb-14 p-1.5 rounded-lg my-4 block" type="email" name="email" />
+		</label>
+		<label class="my-9 text-2xl">
+			Tell me about your project:
+			<textarea class="mt-9 mb-14 rounded-lg block p-1.5" name="message" />
+		</label>
+		<Button buttonType="submit" buttonText="Send" />
+	</form>
 </div>
-<form action="" class="pt-2">
-	<div class="content-container">
-		<label class="my-9 text-2xl" for="email">Email:</label>
-		<input class="mt-9 mb-14 p-1.5 rounded-lg my-4 block" type="text" id="email" />
-		<label class="my-9 text-2xl" for="phone">Phone #</label>
-		<input class="mt-9 mb-14 p-1.5 rounded-lg my-4 block" type="text" id="phone" />
-		<label class="my-9 text-2xl" for="phone">Tell me about your project?</label>
-		<textarea
-			class="mt-9 mb-14 rounded-lg block p-1.5"
-			name="message"
-			id="message"
-			cols="30"
-			rows="6"
-		/>
-		<Link buttonText="Submit" href="" />
-	</div>
-</form>
 
-<!-- </div> -->
 <style>
 	textarea,
 	input {
