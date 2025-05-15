f739d25522f0ddbf8d70d9d42ded321da5321ef6
B33Gs <speschk@yahoo.com>
2025-05-15 19:15:17 -0400

    spelling and grammar updates to portfolio data

--- src/data/case-studies/portfolio.ts
+++ src/data/case-studies/portfolio.ts
@@ -6,12 +6,12 @@ const portfolio: CaseStudy = {
 	preview:
 		"A serverless SvelteKit app that serves as an introduction and contact point as well as somewhere to show off my work.",
 	summary:
-		"A serverless TypeScript Application that uses Tailwind to streamline component styling.  It utilizes file based routing to inject desriptions into components resulting in minimal code for repetetive content.",
+		"A serverless TypeScript Application that uses Tailwind to streamline component styling.  It utilizes file based routing to inject descriptions into components resulting in minimal code for repetitive content.",
 	logo: "",
 	purpose:
 		"This was an opportunity to learn a framework I'd heard a lot of positive things about in SvelteKit and to refine my TypeScript skills.",
 	objective:
-		"Create a portfolio application using SvelteKit and TypeScript so I could show off my projects and continue to refine and expand my skillset.",
+		"Create a portfolio application using SvelteKit and TypeScript so I could show off my projects and continue to refine and expand my skill set.",
 	process: [
 		{
 			step: "Learning SvelteKit",
@@ -24,7 +24,7 @@ const portfolio: CaseStudy = {
 			step: "Theming",
 			text: [
 				"One of the first issues I ran into was how to implement color theming.  In frameworks without component based CSS, I could declare a variable in styles.css and use them from wherever I wanted, but that approach wouldn't work here.",
-				"After a bit of searching online, I used the style attributed of the div wrapping the app and declared the variables there.  Once that was done I could freely use them in the style portion of my components."
+				"After a bit of searching online, I used the style attribute of the div wrapping the app and declared the variables there.  Once that was done I could freely use them in the style portion of my components."
 			]
 		},
 		{
